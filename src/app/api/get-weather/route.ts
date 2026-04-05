32bffb1a8f4f91ff11f2e96af03cd2d32c1654c1
Tanish Majumdar <142332924+tanish35@users.noreply.github.com>
2026-04-05 12:16:08 +0530

    Fix typo in OpenWeatherMap API URL

--- src/app/api/get-weather/route.ts
+++ src/app/api/get-weather/route.ts
@@ -6,7 +6,7 @@ export async function GET() {
     console.log(apiKey);
 
     const response = await fetch(
-      'https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=YOUR_API_KEY'
+      'https://api.openweathermappr.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=YOUR_API_KEY'
     );
 
     const weatherData = await response.json();
